150bd97c1c2dca4177fd3ecf6379396ab642d0ef
jsegelh <jsegelh@siue.edu>
2026-02-15 08:17:32 -0600

    Added a start screen and increased the number of baskets to four

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour{
+    public void StartGame(){
+        SceneManager.LoadScene("_Scene_0");
+    }
+}
